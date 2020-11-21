63832667f3cdada589dbb298bfe950f65728764d
Callum Herries <callum.herries@uber.com>
2020-11-21 18:39:34 +0100

    Complete Queries section

--- src/main/app/client.cljs
+++ src/main/app/client.cljs
@@ -6,8 +6,31 @@
 
 (defonce app (app/fulcro-app))
 
+(defsc ItemList [this props]
+  {:query         [{:item-list/all-items []}]
+   :initial-state {:item-list/all-items []}
+   :ident         (fn [] [:component/id ::item-list])}
+  (dom/p
+    (pr-str "...")))
+
+(def ui-item-list (comp/factory ItemList))
+
+(defsc Thing [this props]
+  {:query         [:a-thing]
+   :initial-state {:a-thing "Some stuff!"}}
+  (dom/div {:style {:border "1px solid black"}}
+           (dom/p "This is a thing")
+           (pr-str props)))
+
+(def ui-thing (comp/factory Thing #_{:keyfn :thing/id}))
+
 (defsc Root [this props]
-  (dom/div "TODO"))
+  {:query         [{:an-added-level (comp/get-query Thing)}] ;; this tells fulcro that Thing owns the state under :an-added-level.
+   :initial-state (fn [_] {:an-added-level (comp/get-initial-state Thing)})} ;; So we need to use the function type of initial-state.
+  (dom/div
+    (dom/h3 "This is root!")
+    (pr-str props)
+    (ui-thing (:an-added-level props))))
 
 (defn ^:export init
   "Shadow-cljs sets this up to be our entry-point function. See shadow-cljs.edn `:init-fn` in the modules of the main build."
