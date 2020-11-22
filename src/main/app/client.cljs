2a7c5ebf8369513b07e202ff9ccf3d3dff01a1ca
Callum Herries <callum.herries@uber.com>
2020-11-22 09:43:45 +0100

    Complete Mutations section

--- src/main/app/client.cljs
+++ src/main/app/client.cljs
@@ -2,31 +2,36 @@
   (:require
    [com.fulcrologic.fulcro.application :as app]
    [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
-   [com.fulcrologic.fulcro.dom :as dom]))
+   [com.fulcrologic.fulcro.dom :as dom]
+   [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]
+   [com.fulcrologic.fulcro.algorithms.merge :as merge]))
 
 (defonce app (app/fulcro-app))
 
-(defsc ItemList [this props]
-  {:query         [{:item-list/all-items []}]
-   :initial-state {:item-list/all-items []}
-   :ident         (fn [] [:component/id ::item-list])}
-  (dom/p
-    (pr-str "...")))
-
-(def ui-item-list (comp/factory ItemList))
+(defmutation add-thing
+  "Mutation: Add a thing to :a-list-of-things"
+  [params]
+  (action [{:keys [state]}]
+          (println (pr-str @state))
+          (let [class (comp/registry-key->class :app.client/Thing)]
+            (swap! state merge/merge-component class {:a-thing "four stuff"}
+                   :append [:a-list-of-things]))))
 
 (defsc Thing [this props]
-  {:query [:a-thing]}
+  {:query [:a-thing :fav-colour]
+   :ident :a-thing}
   (dom/div {:style {:border "1px solid black"}}
            (dom/p "This is a thing")
            (pr-str props)))
 
-(def ui-thing (comp/factory Thing #_{:keyfn :thing/id}))
+(def ui-thing (comp/factory Thing {:keyfn :a-thing}))
 
 (defsc Root [this props]
   {:query         [{:a-list-of-things (comp/get-query Thing)}] ;; this tells fulcro that Thing owns the state under :an-added-level.
-   :initial-state (fn [_] {:a-list-of-things [{:a-thing "1 stuff"}
-                                              {:a-thing "2 stuff"}]})}
+   :initial-state (fn [_] {:a-list-of-things [{:a-thing    "1 stuff"
+                                               :fav-colour "red"}
+                                              {:a-thing    "2 stuff"
+                                               :fav-colour "blue"}]})}
   (dom/div
     (dom/h3 "This is root!")
     (pr-str props)
@@ -46,3 +51,9 @@
   ;; As of Fulcro 3.3.0, this addition will help with stale queries when using dynamic routing:
   (comp/refresh-dynamic-queries! app)
   (js/console.log "Hot reload"))
+
+(comment
+  (pr-str "hey")
+  (+ 3 (+ 1 2))
+  (comp/transact! app [(add-thing {})])
+)
